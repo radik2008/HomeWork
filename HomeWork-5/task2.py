69756ecb2cea209b8f4374fb91118c7d65a2be84
RODION <radik2008@mail.ru>
2023-03-27 22:04:42 +0500

    вторая задача

---
+++ HomeWork-5/task2.py
@@ -0,0 +1,26 @@
+# Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел.
+# Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
+#
+# *Пример:*
+#
+# 2 2    4
+import time
+
+a = int(input("Input number A:"))
+b = int(input("Input number B:"))
+
+
+def sum(n: int, m: int) -> int:
+    if n == 0:
+        return m
+    elif m == 0:
+        return n
+    else:
+        return sum(n - 1, m + 1)
+
+
+
+start = time.time()
+print(sum(a, b))
+finish = time.time()
+print(start - finish)
